12ab5f852e5f028fd2dd618bd9f3db4c0a3efc9e
Biraj Adhikari <adhikari.biraj1@gmail.com>
2023-10-17 21:44:03 -0500

    moved code from main to different library

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,13 @@
-use std::{ error::Error };
 use std::env;
-use colored::*;
 // use rand::{Rng};
 
-mod flashcards;
-use colored::Colorize;
-use flashcards::{ Card, CardList };
-use some_proj::user_input;
-
-fn example(file_name: String) -> Result<CardList, Box<dyn Error>> {
-    let mut contents = csv::Reader::from_path(file_name)
-        .expect("Something went wrong reading the file");
-
-    let mut flashcard_list = CardList::new();
-
-    for record in contents.deserialize() {
-        let card : Card = record?;
-        flashcard_list.add(card);
-    }
-    // flashcardList.print_all_cards();
-    Ok(flashcard_list)
-}
+use some_proj::{ test_my_knowledge };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // let mut rng = rand::thread_rng();
     if args.len() > 1 {
         let file_name = &args[1];
-        let mut all_cards = match example(file_name.to_string()) {
-            Ok(cardlist) => cardlist,
-            Err(e) => panic!("{}", e),
-        };
-        all_cards.shuffle();
-
-        let prompt: String = format!("How many vocabs do you want to try out of {}?: ", all_cards.len());
-        let mut total: u32 = match user_input(prompt, "blue").parse() {
-            Ok(i) => i,
-            Err(e) => panic!("{e}"),
-        };
-        let count = total as usize;
-
-        let mut for_review: Vec<&Card> = Vec::new();
-
-        for card in all_cards.test_knowledge() {
-            if total == 0 {
-                break;
-            }
-            total -= 1;
-            print!("{} ", &card.term.red().bold());
-            let right = user_input(String::from("y/n?:"), "green");
-            if right != "" && right != "y" {
-                for_review.push(&card);
-            }
-            println!("{}\n", &card.meaning);
-        }
-        let review_len: usize = for_review.len();
-
-        if review_len == 0 {
-            println!("Congratulations, you got all {count} terms right.");
-        } else {
-            print!("You got {} terms wrong out of {}, ", review_len, count);
-            println!("Do you want to review those words?");
-            let right = user_input(String::from("y/n?:"), "green");
-            if right == "y" {
-                for card in for_review {
-                    println!("{} => {}", card.term, card.meaning);
-                }
-            }
-        }
+        test_my_knowledge(file_name)
     }
 }
